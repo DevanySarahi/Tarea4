2bbda6418654baeb98562baac116160578325fbc
DevanySarahi <devanyspuente@gmail.com>
2025-03-22 20:15:48 -0600

    Actualización001
    Actualización tarea

---
+++ Producto.java
@@ -0,0 +1,14 @@
+public abstract class Producto {
+    protected String nombre;
+    protected double precio;
+    protected int cantidad;
+
+    public Producto(String nombre, double precio, int cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public abstract void mostrarInformacion();
+    public abstract double calcularValorInventario();
+}
